fd7de9554b7205262c6041ab1cf1f71d32c61469
ivanrmrhdz <ivanrmrhdz@gmail.com>
2025-01-29 11:26:37 -0600

    Ticket Tool Project Excercise - from https://www.youtube.com/@LearningPartnerDigital

---
+++ src/app/pages/child-category/child-category.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-child-category',
+  imports: [],
+  templateUrl: './child-category.component.html',
+  styleUrl: './child-category.component.css'
+})
+export class ChildCategoryComponent {
+
+}
---
+++ src/app/pages/parent-category/parent-category.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parent-category',
+  imports: [],
+  templateUrl: './parent-category.component.html',
+  styleUrl: './parent-category.component.css'
+})
+export class ParentCategoryComponent {
+
+}
